18d59b65635a83b98c26708164d79ba4a2ac5472
vikas <vikas@gmail.com>
2021-02-21 17:46:29 +0000

    new folder

---
+++ service-1/app1.py
@@ -0,0 +1,15 @@
+from flask import flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+
+
+
+    return render_template('index.html',animal="", noise="")
+
+
+if __name__ =="__main__":
+ 
